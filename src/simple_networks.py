9bdb04c960b46db82bca9a55c3bb6a1c9222b22c
casxt <maple@forer.cn>
2020-10-27 18:31:44 +0800

    update code

--- src/simple_networks.py
+++ src/simple_networks.py
@@ -159,12 +159,12 @@ class SimpleCoPINet(nn.Module):
         row_features = self.relu(
             self.bn_row(self.conv_row(torch.cat((row1_features, row3_features), dim=0))))
 
-        row1 = row_features[:N, :, :, :].unsqueeze(1).unsqueeze(1)#.expand(N, 1, 1, self.internal_channel, H, W)
+        row1 = row_features[:N, :, :, :].view(N, 1, 1, self.internal_channel, H, W) # .unsqueeze(1).unsqueeze(1)#.expand(N, 1, 1, self.internal_channel, H, W)
         # row2 = row_features[N:2 * N, :, :, :].unsqueeze(1).unsqueeze(1).expand(N, 8, 1, 64, 20, 20)
-        row3 = row_features[N:, :, :, :].view(-1, 1, self.internal_channel, H, W).unsqueeze(2)
-        print("row_features", row_features.shape)
-        print("row1", row1.shape)
-        print("row3", row3.shape)
+        row3 = row_features[N:, :, :, :].view(N, 1, 1, self.internal_channel, H, W)# .unsqueeze(2)
+        # print("row_features", row_features.shape)
+        # print("row1", row1.shape)
+        # print("row3", row3.shape)
         final_row_features = torch.sum(torch.cat((row1, row3), dim=2), dim=2)
 
         # col1_features = torch.sum(input_features[:, 0:9:3, :, :, :], dim=1)  # N, 64, 20, 20
